c013881c0d84a755f6bf99dceca775fde4771160
Jens Willy Johannsen <jens@jenswilly.dk>
2022-02-11 11:35:07 +0100

    Began work on HTTPS POST instead of MQTT

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -29,7 +29,12 @@
 #include "lwip/netdb.h"
 
 #include "esp_log.h"
-#include "mqtt_client.h"
+// #include "mqtt_client.h"
+
+// HTTP client
+#include "esp_http_client.h"
+#include "esp_tls.h"
+#include "esp_crt_bundle.h"
 
 #include "decode_raw_mobilenet.h"
 #include "esp32_spi_impl.h"
@@ -51,11 +56,15 @@ static const char *TAG = "MQTT_EXAMPLE";
 #define MQTT_PREFIX "esp32-depthai"
 
 /// MQTT topics
-static const std::string mqtt_ping_topic = MQTT_PREFIX "/ping/" CONFIG_BROKER_CLIENTID;
-static const std::string mqtt_detections_topic = MQTT_PREFIX "/detections/" CONFIG_BROKER_CLIENTID;
+// static const std::string mqtt_ping_topic = MQTT_PREFIX "/ping/" CONFIG_BROKER_CLIENTID;
+// static const std::string mqtt_detections_topic = MQTT_PREFIX "/detections/" CONFIG_BROKER_CLIENTID;
 
 /// Global MQTT client handle
-esp_mqtt_client_handle_t client;
+//esp_mqtt_client_handle_t client;
+
+#define MAX_HTTP_RECV_BUFFER 512
+#define MAX_HTTP_OUTPUT_BUFFER 2048
+
 
 /// Timestamp for throttling
 auto lastTimestamp = std::chrono::steady_clock::now();
@@ -115,6 +124,7 @@ static std::vector<Rect> slots = {
     {0.5, 0, 1, 1}  // Right half
 };
 
+#if 0
 /*
  * @brief Event handler registered to receive MQTT events
  *
@@ -212,6 +222,82 @@ static void mqtt_app_start(void)
     esp_mqtt_client_register_event(client, MQTT_EVENT_ANY, mqtt_event_handler, NULL);
     esp_mqtt_client_start(client);
 }
+#endif
+
+esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+{
+    static char *output_buffer;  // Buffer to store response of http request from event handler
+    static int output_len;       // Stores number of bytes read
+    switch(evt->event_id) {
+        case HTTP_EVENT_ERROR:
+            ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+            break;
+
+        case HTTP_EVENT_ON_CONNECTED:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+            break;
+
+        case HTTP_EVENT_HEADER_SENT:
+            ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+            break;
+
+        case HTTP_EVENT_ON_HEADER:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+            break;
+
+        case HTTP_EVENT_ON_DATA:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+            /*
+             *  Check for chunked encoding is added as the URL for chunked encoding used in this example returns binary data.
+             *  However, event handler can also be used in case chunked encoding is used.
+             */
+            if (!esp_http_client_is_chunked_response(evt->client)) {
+                // If user_data buffer is configured, copy the response into the buffer
+                if (evt->user_data) {
+                    memcpy(evt->user_data + output_len, evt->data, evt->data_len);
+                } else {
+                    if (output_buffer == NULL) {
+                        output_buffer = (char *) malloc(esp_http_client_get_content_length(evt->client));
+                        output_len = 0;
+                        if (output_buffer == NULL) {
+                            ESP_LOGE(TAG, "Failed to allocate memory for output buffer");
+                            return ESP_FAIL;
+                        }
+                    }
+                    memcpy(output_buffer + output_len, evt->data, evt->data_len);
+                }
+                output_len += evt->data_len;
+            }
+            break;
+
+        case HTTP_EVENT_ON_FINISH:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+            if (output_buffer != NULL) {
+                // Response is accumulated in output_buffer. Uncomment the below line to print the accumulated response
+                // ESP_LOG_BUFFER_HEX(TAG, output_buffer, output_len);
+                free(output_buffer);
+                output_buffer = NULL;
+            }
+            output_len = 0;
+            break;
+
+        case HTTP_EVENT_DISCONNECTED:
+            ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+            int mbedtls_err = 0;
+            esp_err_t err = esp_tls_get_and_clear_last_error((esp_tls_error_handle_t)(evt->data), &mbedtls_err, NULL);
+            if (err != 0) {
+                if (output_buffer != NULL) {
+                    free(output_buffer);
+                    output_buffer = NULL;
+                }
+                output_len = 0;
+                ESP_LOGI(TAG, "Last esp error code: 0x%x", err);
+                ESP_LOGI(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
+            }
+            break;
+    }
+    return ESP_OK;
+}
 
 void run_demo() {
 	uint8_t req_success = 0;
@@ -290,7 +376,7 @@ void run_demo() {
 #else
                         // Only send if not running in debug mode
                         std::string jsonString = jsonRoot.dump();
-                        esp_mqtt_client_publish(client, mqtt_detections_topic.c_str(), jsonString.c_str(), jsonString.length(), 0, 0);
+                        // esp_mqtt_client_publish(client, mqtt_detections_topic.c_str(), jsonString.c_str(), jsonString.length(), 0, 0);
 #endif
                 }
 			}
@@ -328,8 +414,8 @@ void app_main(void)
     ESP_ERROR_CHECK(example_connect());
 
     // Connect to MQTT broker
-    mqtt_app_start();
-    ESP_LOGI(TAG, "MQTT client started...");
+//    mqtt_app_start();
+//    ESP_LOGI(TAG, "MQTT client started...");
 
     // Init spi for the esp32 and send pin
     init_esp32_spi();
